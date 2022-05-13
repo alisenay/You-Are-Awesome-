c66b8e23319e4f6ddc209a7cf455eb7731b5d64d
Ali Şenay <alisenay95@hotmail.com>
2022-05-13 23:32:53 +0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by MacbookPro_Ali Şenay on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
